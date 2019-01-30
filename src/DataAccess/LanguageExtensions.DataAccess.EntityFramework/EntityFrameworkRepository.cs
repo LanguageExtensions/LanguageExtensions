ef774177f3c8d92ade8d4447edf1e4bb8357f31f
Piyush Rathi <jst4upiyush@gmail.com>
2019-01-31 04:20:47 +0530

    Intermitent changes for Entity framework Repository

---
+++ src/DataAccess/LanguageExtensions.DataAccess.EntityFramework/EntityFrameworkRepository.cs
@@ -0,0 +1,39 @@
+﻿using LanguageExtensions.DataAccess.Abstractions;
+using LanguageExtensions.Specifications;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace LanguageExtensions.DataAccess.EntityFramework
+{
+    public class EntityFrameworkRepository<TEntity, TKey> : IGetRepository<TEntity, TKey> where TEntity : class
+    {
+        private readonly IDbSet<TEntity> _dbSet;
+        private readonly DbContext _dbContext;
+        private readonly Expression<Func<TEntity, TKey>> _primaryKeySelector;
+
+        public EntityFrameworkRepository(DbContext dbContext, Expression<Func<TEntity, TKey>> primaryKeySelector)
+        {
+            _dbContext = dbContext;
+            _dbSet = _dbContext.Set<TEntity>();
+            _primaryKeySelector = primaryKeySelector;
+        }
+
+        public async Task<TEntity> GetAsync(TKey key)
+        {
+            var spec = GetPrimaryKeySpecification(key);
+            return await _dbSet.FirstOrDefaultAsync();
+        }
+
+        public Task<IEnumerable<TEntity>> GetManyAsync(IEnumerable<TKey> keys) => throw new NotImplementedException();
+
+        #region Private Helper Methods
+
+        private Specification<TEntity> GetPrimaryKeySpecification(TKey key)
+            => new PropertySpecification<TEntity, TKey>(_primaryKeySelector, key);
+
+        #endregion
+    }
+}
