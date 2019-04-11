e98d2c38f4e140fe0a9d862a6b7e7bf438064260
jst4upiyush <jst4upiyush@gmail.com>
2019-04-11 22:53:53 +0530

    Restructuring of resources to make it more maintainable

--- src/AspNetCore/LanguageExtensions.AspNetCore/Middlewares/CorrelationId/CorrelationIdServiceExtensions.cs
+++ src/AspNetCore/LanguageExtensions.AspNetCore/Middlewares/CorrelationId/CorrelationIdServiceExtensions.cs
@@ -1,11 +1,20 @@
 ﻿using LanguageExtensions.AspNetCore.Middlewares.CorrelationId;
 using LanguageExtensions.Correlation;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using System;
 
 namespace Microsoft.AspNetCore.Builder
 {
     public static class CorrelationIdServiceExtensions
     {
+        public static IServiceCollection AddCorrelationContext(this IServiceCollection serviceCollection)
+        {
+            serviceCollection.TryAddSingleton<ICorrelationContextAccessor, CorrelationContextAccessor>();
+            serviceCollection.AddTransient(s => s.GetService<ICorrelationContextAccessor>().CorrelationContext);
+            return serviceCollection;
+        }
+
         public static IApplicationBuilder UseCorrelationId(this IApplicationBuilder app)
         {
             if (app.ApplicationServices.GetService(typeof(ICorrelationContextAccessor)) == null)
--- src/Specifications/LanguageExtensions.Specifications.Tests/SpecificationTest.cs
+++ src/Specifications/LanguageExtensions.Specifications.Tests/SpecificationTest.cs
@@ -6,7 +6,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Linq.Expressions;
 
-namespace LanguageExtensions.Tests.Specifications
+namespace LanguageExtensions.Specifications.Tests
 {
     public class SpecificationTest
     {
